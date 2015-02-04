e996af5bd0f20fc3408e34c75dd49f12c6253eac
chengzh2008 <chengzh2008@gmail.com>
2015-02-03 21:32:58 -0800

    add .jscsrc and jscs task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,45 @@
 "use strict";
 
-module.exports = function(grunt) {
-  // let grunt load npm tasks
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-simple-mocha');
+module.exports = function (grunt) {
+    // let grunt load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-jscs');
 
-  // grunt initilization
-  grunt.initConfig({
-    jshint: {
-      dev: {
-        options: {
-          node: true,
-          globals: {
-            describe: true,
-            it: true,
-            before: true,
-            after: true
-          }
+    // grunt initilization
+    grunt.initConfig({
+        jshint: {
+            dev: {
+                options: {
+                    node: true,
+                    globals: {
+                        describe: true,
+                        it: true,
+                        before: true,
+                        after: true
+                    }
+                },
+                src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
+            }
         },
-        src: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js']
-      }
-    },
-    simplemocha: {
-      all: {
-        src: ['test/**/*.js']
-      }
-    }
-  });
+        jscs: {
+            all: {
+                options: {
+                    config: ".jscsrc"
+                },
+                files: {
+                    src: ['Gruntfile.js', 'lib', 'test', 'index.js']
+                }
+            }
+        },
+        simplemocha: {
+            all: {
+                src: ['test/**/*.js']
+            }
+        }
+    });
 
-  // register the tasks
-  grunt.registerTask('test', ['jshint', 'simplemocha']);
-  grunt.registerTask('default', ['test']);
+    // register the tasks
+    grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
+    grunt.registerTask('default', ['test']);
 };
--- test/myAppTest.js
+++ test/myAppTest.js
@@ -1,29 +1,29 @@
 "use strict";
 
-var myApp = require('../lib/greet');
-var expect = require('chai').expect;
+var myApp = require('../lib/greet'),
+    expect = require('chai').expect;
 
-describe('myApp from greet.js', function(){
-  it('should take input and greet input', function(){
-    expect(myApp("world")).to.eql("hello world");
-  });
-
-  it('should grab input from process.argv', function(){
-    var arg_cache;
-    var somename = 'cat';
-    before(function(){
-      // cache the current process.argv
-      arg_cache = process.argv;
-      process.argv[2] = somename;
+describe('myApp from greet.js', function () {
+    it('should take input and greet input', function () {
+        expect(myApp("world")).to.eql("hello world");
     });
 
-    it('should grab soemname from process.argv', function() {
-      expect(myApp()).to.eql("hello " + somename);
-    });
+    it('should grab input from process.argv', function () {
+        var arg_cache,
+            somename = 'cat';
+        before(function () {
+            // cache the current process.argv
+            arg_cache = process.argv;
+            process.argv[2] = somename;
+        });
 
-    // restore the process.argv
-    after(function(){
-      process.argv = arg_cache;
+        it('should grab soemname from process.argv', function () {
+            expect(myApp()).to.eql("hello " + somename);
+        });
+
+        // restore the process.argv
+        after(function () {
+            process.argv = arg_cache;
+        });
     });
-  });
 });
