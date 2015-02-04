ec95c7a71ba22e36608c1d47acd32bebe67a66d1
chengzh2008 <chengzh2008@gmail.com>
2015-02-03 22:03:55 -0800

    add grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,19 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // grunt initilization
     grunt.initConfig({
+        watch: {
+            scripts: {
+                files: ['lib/*.js', 'Gruntfile.js', 'lib/**/*.js', 'test/**/*.js', 'index.js', '.jscsrc'],
+                tasks: ['default'],
+                options: {
+                    spawn: true
+                }
+            }
+        },
         jshint: {
             dev: {
                 options: {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var myApp = require('./lib/greet');
+var myApp = require('./lib/greet'),
 
 module.exports = myApp;
